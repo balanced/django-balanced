77b56fcf05a98cbd6c7d9a157600bff420256fed
Balanced Marshall <marshall@poundpay.com>
2012-11-29 18:04:18 -0800

    fix missing file

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def _get_version():
 
 
 def _get_long_description():
-    path = os.path.join(PATH_TO_FILE, 'README.md')
+    path = os.path.join(PATH_TO_FILE, 'README')
     fo = open(path)
     try:
         return fo.read()
